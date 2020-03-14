66543734bc994e97418537355c601c2212ad1a29
Broc Gailit <brocgailit@gmail.com>
2020-03-13 17:27:14 -0700

    Test posting to vinti endpoint

--- Controller/VintiFourPayments.php
+++ Controller/VintiFourPayments.php
@@ -3,6 +3,7 @@
 namespace VintiFour\Controller;
 
 use GuzzleHttp\Client;
+use GuzzleHttp\Exception\ClientException;
 use \LimeExtra\Controller;
 use VintiFour\Controller\Endpoint;
 
@@ -40,11 +41,36 @@ class VintiFourPayments extends Controller {
 			$data .= $_POST['referenceNumber'];
 		}
 
-		return base64_encode(hash("sha512", $data, true));
+		$fingerprint = base64_encode(hash("sha512", $data, true));
+
+
+		try {
+			$res = $this->client->request('POST', '', [
+				'form_params' => [
+					'transactionCode' => $_POST['transactionCode'] ?? 1,
+					'posID' => $this->config['posID'],
+					'amount' => $_POST['amount'],
+					'currency' => $_POST['currency'] ?? 132,
+					'is3DSec' => 1,
+					'urlMerchantResponse' => $this->config['urlMerchantResponse'],
+					'fingerPrint' => $fingerprint,
+					'fingerPrintVersion' => 1,
+					'languageMessages' => 'en',
+					'merchantRef' => $_POST['merchantRef'],
+					'merchantSession' => $_POST['merchantSession'],
+					'timestamp' => $_POST['timestamp'],
+					'posAutCode' => $this->config['posAutCode'],
+				]
+			]);
+			return $res->getBody();
+		} catch(ClientException $e) {
+				$response = $e->getResponse();
+				return $response->getBody()->getContents();
+		}
 	}
 
 	public function response() {
-		return 'Why hello there!';
+		return var_dump($_GET);
 	}
 
 }
