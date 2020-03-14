e55948bc4dc01ab454bd40bcb9501c95f5787046
Broc Gailit <brocgailit@gmail.com>
2020-03-14 11:20:00 -0700

    Add vinti form to payment endpoint

--- Controller/VintiFourPayments.php
+++ Controller/VintiFourPayments.php
@@ -45,7 +45,43 @@ class VintiFourPayments extends Controller {
 	}
 
 	public function payment() {
-		return '<html><head><title>Vinti4 Payment</title><script>alert("hello");</script></head><body>Loading...</body></html>';
+
+		//fallback vars
+		$fingerPrintVersion = $_GET['fingerPrintVersion'] ?? 1;
+		$currency = $_GET['curency'] ?? 132;
+		$is3DSec = $_GET['is3DSec'] ?? 1;
+		$language = $_GET['language'] ?? 'en';
+
+		//form setup
+		$formUrl = 'https://mc.vinti4net.cv/Client_VbV_v2/biz_vbv_clientdata.jsp?';
+		$formUrl .= "FingerPrint={$_GET['fingerPrint']}";
+		$formUrl .= "&TimeStamp={$_GET['timestamp']}";
+		$formUrl .= "&FingerPrintVersion={$fingerPrintVersion}";
+
+		//page setup
+		$page = '<html>';
+		$page .= '<head>';
+		$page .= '  <title>Vinti4 Payment</title>';
+		$page .= '</head>';
+		$page .= '<body>';
+		$page .= "  <form id=\"vintiPaymentForm\" action=\"{$formUrl}\">";
+		$page .= "    <input type=\"hidden\" name=\"transactionCode\" value=\"1\" />";
+		$page .= "    <input type=\"hidden\" name=\"posID\" value=\"{$this->config['posID']}\" />";
+		$page .= "    <input type=\"hidden\" name=\"amount\" value=\"{$_GET['amount']}\" />";
+		$page .= "    <input type=\"hidden\" name=\"currency\" value=\"{$currency}\" />";
+		$page .= "    <input type=\"hidden\" name=\"is3DSec\" value=\"{$is3DSec}\" />";
+		$page .= "    <input type=\"hidden\" name=\"urlMerchantResponse\" value=\"{$_GET['urlMerchantResponse']}\" />";
+		$page .= "    <input type=\"hidden\" name=\"fingerPrint\" value=\"{$_GET['fingerPrint']}\" />";
+		$page .= "    <input type=\"hidden\" name=\"fingerPrintVersion\" value=\"{$fingerPrintVersion}\" />";
+		$page .= "    <input type=\"hidden\" name=\"languageMessages\" value=\"{$language}\" />";
+		$page .= "    <input type=\"hidden\" name=\"merchantRef\" value=\"{$_GET['merchantRef']}\" />";
+		$page .= "    <input type=\"hidden\" name=\"merchantSession\" value=\"{$_GET['merchantSession']}\" />";
+		$page .= "    <input type=\"hidden\" name=\"timestamp\" value=\"{$_GET['timestamp']}\" />";
+		$page .= '  </form>';
+		$page .= '  Loading...';
+		$page .= '  <script>document.getElementById("vintiPaymentForm").submit()</script>';
+		$page .= '</body>';
+		$page .= '</html>';
 	}
 
 	public function response() {
